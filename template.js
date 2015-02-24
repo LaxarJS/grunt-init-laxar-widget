c9e90d35affcf9a517e855102abeb6c315ad926d
Michael Kurze <michael.kurze@aixigo.de>
2015-02-24 14:39:39 +0100

    fixup! (#5) fix hyphen support in artifact name

--- template.js
+++ template.js
@@ -5,19 +5,21 @@
  */
 'use strict';
 
+var artifactType = 'widget';
+
 // Basic template description.
-exports.description = 'Create a LaxarJS widget, including Jasmine specs';
+exports.description = 'Create a LaxarJS ' + artifactType + ', including Jasmine specs';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = 'For more information about LaxarJS artifacts, ' +
-  'please see the docs at ' +
-  'https://github.com/LaxarJS/laxar/blob/master/docs/core_concepts.md';
+   'please see the docs at ' +
+   'https://github.com/LaxarJS/laxar/blob/master/docs/concepts.md#laxarjs-concepts';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You can now start developing your widget! For more information ' +
    'about developing widgets with LaxarJS, please refer to the manuals:' +
    '\n\n' +
-   'http://laxarjs.org/docs/manuals/index.md';
+   'https://github.com/LaxarJS/laxar/blob/master/docs/manuals/index.md#manuals';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
@@ -25,7 +27,7 @@ exports.warnOn = '*';
 // The actual init template.
 exports.template = function( grunt, init, done ) {
 
-   var options = { type: 'widget' };
+   var options = { type: artifactType };
    var prompts = require( './prompts' )( options, init );
 
    init.process( options, prompts, function( err, props ) {
